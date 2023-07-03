78512d33d3ddb3d4aac42b4b365fa3729725c50b
sidp16 <siddharth.prasad1609@gmail.com>
2023-07-03 19:51:11 +0100

    Can extract relevant batting data from scorecard

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-print("Hello world!")
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+
+def extract_batting_data(series_id, match_id):
+    URL = (
+        "https://www.espncricinfo.com/series/"
+        + str(series_id)
+        + "/scorecard/"
+        + str(match_id)
+    )
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "lxml")
+
+    table_body = soup.find_all("tbody")
+    batsmen_df = pd.DataFrame(
+        columns=["Name", "Desc", "Runs", "Balls", "4s", "6s", "SR", "Team"]
+    )
+    for i, table in enumerate(table_body[0:4:2]):
+        rows = table.find_all("tr")
+        for row in rows:
+            cols = row.find_all("td")
+            cols = [x.text.strip().replace("\xa0", " ") for x in cols]
+            if cols[0] == "Extras" or cols[0] == "TOTAL":
+                continue
+            if len(cols) == 8:
+                batsmen_df = batsmen_df.append(
+                    pd.Series(
+                        [
+                            re.sub(r"\W+", " ", cols[0].split("(c)")[0]).strip(),
+                            cols[1],
+                            cols[2],
+                            cols[3],
+                            cols[5],
+                            cols[6],
+                            cols[7],
+                            i + 1,
+                        ],
+                        index=batsmen_df.columns,
+                    ),
+                    ignore_index=True,
+                )
+
+    print(batsmen_df)
+    return batsmen_df
+
+
+if __name__ == "__main__":
+    extract_batting_data(8048, 1136561)
+    extract_batting_data(1298134, 1298150)

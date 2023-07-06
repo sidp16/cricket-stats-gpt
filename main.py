12553fa281bc4d9dae48ad61f46925fbbacdf918
sidp16 <siddharth.prasad1609@gmail.com>
2023-07-06 23:56:33 +0100

    Can question GPT about dataset

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import openai
+from credentials import gpt_api_key
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from espncricinfo.match import Match
 
 
 def extract_batting_data(series_id, match_id):
@@ -42,7 +50,6 @@ def extract_batting_data(series_id, match_id):
                     ),
                     ignore_index=True,
                 )
-
     return batsmen_df
 
 
@@ -99,6 +106,7 @@ def extract_bowling_data(series_id, match_id):
                     ),
                     ignore_index=True,
                 )
+
     return bowler_df
 
 
@@ -108,11 +116,33 @@ if __name__ == "__main__":
     # extract_bowling_data(8048, 1136561)
 
     # IND V PAK 2022 T20 WC
-    # pakvind_batting = extract_batting_data(1298134, 1298150)
+    m = Match("1298150")
+    filename = (
+        f"{m._team_1_abbreviation()} vs {m._team_2_abbreviation()} on {m._date()}"
+    )
+    pakvind_batting = extract_batting_data(1298134, 1298150)
+    pakvind_batting.to_csv(filename, index=False)
     # pakvind_bowling = extract_bowling_data(1298134, 1298150)
-    # print(pakvind_batting)
 
-    # IND V ENG 2018 1st Test
-    # indveng_batting = extract_batting_data(1119528, 1119549)
-    indveng_bowling = extract_bowling_data(1119528, 1119549)
-    print(indveng_bowling)
+    # Testing for an ODI: IND V SL 2011 ODI WC FINAL
+    # indvsl_batting = extract_batting_data(381449, 433606)
+    # indvsl_bowling = extract_bowling_data(381449, 433606)
+
+    # Testing for a Test with 4 Innings: IND V ENG 2018 1st Test
+    # indveng_batting18 = extract_batting_data(1119528, 1119549)
+    # indveng_bowling18 = extract_bowling_data(1119528, 1119549)
+
+    # Testing for a Test with 3 Innings: IND V ENG 2021 4th Test
+    # indveng_batting21 = extract_batting_data(1243364, 1243387)
+    # indveng_bowling21 = extract_bowling_data(1243364, 1243387)
+
+    # Testing for a Test with 2 Innings: SA V ENG 2000 5th Test
+    # engvsa_batting = extract_batting_data(61687, 63864)
+    # engvsa_bowling = extract_bowling_data(61687, 63864)
+
+    openai.api_key = gpt_api_key
+    df = pd.read_csv("IND vs PAK on 2022-10-23")
+    chat = ChatOpenAI(
+        model_name="gpt-3.5-turbo", temperature=0.0, openai_api_key=gpt_api_key
+    )
+    agent = create_pandas_dataframe_agent(chat, df, verbose=True)
